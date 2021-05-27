af686351bbfda898c07d1575c40720da9712eac9
santiago <santiagosalas.6532@gmail.com>
2021-05-27 00:06:39 -0500

    Creacion del servicio con plantillas thymeleaf, funcionalidad listar personas hecha, insercion manual desde la DB.

---
+++ src/main/java/com/arquitecturajava/springbootthymeleaf/Persona.java
@@ -0,0 +1,40 @@
+package com.arquitecturajava.springbootthymeleaf;
+
+public class Persona {
+
+	private String nombre;
+	private String apellidos;
+	private int edad;
+
+	//GETTER SETTERS
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	//CONSTRUCTORS
+	public Persona(String nombre, String apellidos, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+	}
+	public Persona() {
+		super();
+	}
+
+}
