73305e1429d0b2f3a7cc9cd61056c958b68f9154
santiago <santiagosalas.6532@gmail.com>
2021-05-30 23:02:23 -0500

    Ajustes, nueva arquitectura implementada convertido en CLIENTE

--- src/main/java/com/arquitecturajava/springbootthymeleaf/SpringBootServer.java
+++ src/main/java/com/arquitecturajava/springbootthymeleaf/SpringBootServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootthymeleafApplication {
+public class SpringBootServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootthymeleafApplication.class, args);
+		SpringApplication.run(SpringBootServer.class, args);
 	}
 
 }
--- src/test/java/com/arquitecturajava/springbootthymeleaf/SpringBootServerTests.java
+++ src/test/java/com/arquitecturajava/springbootthymeleaf/SpringBootServerTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootthymeleafApplicationTests {
+public class SpringBootServerTests {
 
 	@Test
 	public void contextLoads() {
